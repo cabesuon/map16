237a918098ee9fcc97daeb1929da37edef7ead59
cabesuon <manuelmartinezluz@gmail.com>
2020-12-22 10:40:49 -0300

    feat: update log to soracom import data script

--- works/php/src/soracom/import-data-live-2min.php
+++ works/php/src/soracom/import-data-live-2min.php
@@ -4,7 +4,7 @@ error_log(
   implode(
     " ",
     array_merge(
-      ["***** "],
+      ["xxxxx"],
       array_map(
         function($v) { return "$v:$_SERVER[$v]"; },
         [
@@ -13,8 +13,7 @@ error_log(
           "CONTENT_TYPE",
           "REMOTE_ADDR",
           "REMOTE_HOST",
-          "REMOTE_PORT",
-          "REMOTE_USER"
+          "REMOTE_PORT"
         ]
       )
     )
@@ -23,14 +22,14 @@ error_log(
 
 //Make sure that it is a POST request.
 if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0){
-  error_log("***** ERROR: Request method must be POST");
+  error_log("xxxxx ERROR: Request method must be POST");
   exit(1);
 }
 
 //Make sure that the content type of the POST request has been set to application/json
 $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
 if(strcasecmp($contentType, 'application/json') != 0){
-  error_log("***** ERROR: Content type must be: application/json");
+  error_log("xxxxx ERROR: Content type must be: application/json");
   exit(1);
 }
 
@@ -38,7 +37,7 @@ if(strcasecmp($contentType, 'application/json') != 0){
 $content = trim(file_get_contents("php://input"));
 
 error_log(
-  sprintf("***** REQUEST_CONTENT: %s", preg_replace('/\s+/', '', file_get_contents("php://input")))
+  sprintf("xxxxx REQUEST_CONTENT: %s", preg_replace('/\s+/', '', file_get_contents("php://input")))
 );
 
 //Attempt to decode the incoming RAW post data from JSON.
@@ -47,7 +46,7 @@ $decoded = json_decode($content, true);
 //If json_decode failed, the JSON is invalid.
 // if(!is_object($decoded)){
   // throw new Exception('Received content contained invalid JSON');
-  // error_log("***** ERROR: Received content contained invalid JSON");
+  // error_log("xxxxx ERROR: Received content contained invalid JSON");
   // exit(1);
 // }
 
@@ -59,29 +58,29 @@ $password = "map16sqlsys";
 $dbname = "map16_sus_live_sensor_db";
 
 $myConnection = mysqli_connect($servername,$username,$password,$dbname)
-or die ("***** ERROR: Could not connect to mysql"); 
+or die ("xxxxx ERROR: Could not connect to mysql"); 
 
 /* check connection */
 if (mysqli_connect_errno()) {
   error_log(
-    sprintf("***** ERROR: Connect failed: %s", mysqli_connect_error())
+    sprintf("xxxxx ERROR: Connect failed: %s", mysqli_connect_error())
   );
   exit(1);
 }
 
 if (empty($decoded['Data'])) {
   error_log(
-    sprintf("***** ERROR: Received content is empty - ICCID: %s", $decoded['ICCID'])
+    sprintf("xxxxx ERROR: Received content is empty - ICCID: %s", $decoded['ICCID'])
   );
   exit(1);
 }
 
 // fixed values
-$samplingInterval = $decoded['Sampling Interval'];
-$rssi = $decoded['RSSI'];
-$imei = $decoded['IMEI'];
-$iccid = $decoded['ICCID'];
-$network = $decoded['Network'];
+$samplingInterval = isset($decoded['Sampling Interval']) ? $decoded['Sampling Interval'] : '';
+$rssi = isset($decoded['RSSI']) ? $decoded['RSSI'] : '';
+$imei = isset($decoded['IMEI']) ? $decoded['IMEI'] : '';
+$iccid = isset($decoded['ICCID']) ? $decoded['ICCID'] : '';
+$network = isset($decoded['Network']) ? $decoded['Network'] : '';
 $count = count($decoded['Data']);
 $values = [];
 $NETWORK_DELAY = 18;
@@ -89,6 +88,27 @@ $NETWORK_DELAY = 18;
 $subSeconds = $count * $samplingInterval + $NETWORK_DELAY;
 
 for ($i = 0; $i < $count; $i++) {
+  if (!isset($decoded['Data'][$i])) {
+    error_log(
+      sprintf("xxxxx ERROR: Data row %d is empty", $i)
+    );
+    continue;
+  }
+
+  if (!isset($decoded['Data'][$i]['Distance'])) {
+    error_log(
+      sprintf("xxxxx ERROR: Distance in data row %d is empty", $i)
+    );
+    continue;
+  }
+
+  if (!isset($decoded['Data'][$i]['Battery'])) {
+    error_log(
+      sprintf("xxxxx ERROR: Battery in data row %d is empty", $i)
+    );
+    continue;
+  }
+
   $measure = $decoded['Data'][$i]['Distance'];
   $voltage = $decoded['Data'][$i]['Battery'];
   $subSeconds -= $samplingInterval;
@@ -131,12 +151,14 @@ $sqlInsertValues
 
 // store data
 
-# print($sqlCommand);
+$query=mysqli_query($myConnection, $sqlCommand);
 
-$query=mysqli_query($myConnection, $sqlCommand) or
-  die(
-    sprintf("***** ERROR: Could not insert values to store table: %s", mysqli_error($myConnection))
+if (!$query) {
+  error_log(
+    sprintf("xxxxx ERROR: Could not insert values to store table: %s", mysqli_error($myConnection))
   );
+  exit(1);
+}
 
 // live data
 
@@ -157,10 +179,14 @@ network = '$network'
 WHERE iccid = '$iccid' AND date_id <> NOW()
 ";
 
-$query = mysqli_query($myConnection, $sqlCommand) or 
-  die(
-    sprintf("***** ERROR: Could not udpate values to live table: %s", mysqli_error($myConnection))
+$query = mysqli_query($myConnection, $sqlCommand);
+
+if (!$query) {
+  error_log(
+    sprintf("xxxxx ERROR: Could not udpate values to live table: %s", mysqli_error($myConnection))
   );
+  exit(1);
+}
 
 /* close connection */
 mysqli_close($myConnection);
